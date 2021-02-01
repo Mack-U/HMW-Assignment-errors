45c3cb17ced8eeafae3de2edb576ec60c3b90ab0
Donnie Underwood <76881123+Mack-U@users.noreply.github.com>
2021-02-01 16:48:15 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+package helloWorld;
+
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		scanner inputInfo = new Scanner(System.in);
+		// System.out.println("Hello World!!! Finally!");
+	    System.out.println("What is your name?");
+	    String userName = input_info.nextLine();
+	    input info.close();
+	    System.out.printf("Hello %s, nice to meet you.", userName);
+	  
+	}
+
+}
